2265f9e39ce7c69d9883cd11a91115b9786a4a52
NikaShat <nikusenka04@ya.ru>
2020-05-12 16:47:51 +0300

    oi

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ def delete(id):
     session.commit()
     return redirect('/profile/my_tea')
 
+
 @app.route('/editing/<id>', methods=['GET', 'POST'])
 def edit(id):
     if request.method == 'POST':
