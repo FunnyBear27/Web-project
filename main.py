b2f790f0edec6fdc52fbf864381f3f2a174da341
FunnyBear27 <47087854+FunnyBear27@users.noreply.github.com>
2023-01-24 17:32:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-app.config['SECRET_KEY'] = 'dda3ddba-c9ea-4ead-9010-f43fbc15c6e3'
+app.config['SECRET_KEY'] = ''
 # подключение к БД
 con = sqlite3.connect("db/zodiac.db", check_same_thread=False)
 cur = con.cursor()
